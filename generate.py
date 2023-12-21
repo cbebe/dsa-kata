70a26687feb5b2b1f46ce020a4936f554003a542
Charles Ancheta <55412395+cbebe@users.noreply.github.com>
2023-12-20 22:55:37 -0700

    Replace dist_util calls with shutil

--- generate.py
+++ generate.py
@@ -1,11 +1,9 @@
 import sys
-import os
-from distutils.dir_util import copy_tree, remove_tree
+from shutil import copytree, rmtree
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
         print(f"USAGE: {sys.argv[0]} DIR")
         sys.exit(1)
-    if os.path.isdir("template/__pycache__"):
-        remove_tree("template/__pycache__", verbose=True)
-    copy_tree("template", sys.argv[1])
+    rmtree("template/__pycache__", ignore_errors=True)
+    copytree("template", sys.argv[1])
